34660dcffff069ee53117e738758d95db48c18b0
천보라 <capsulejay963@naver.com>
2019-04-30 08:39:35 +0900

    sorry

--- SafeFood_Web_Spring_gj_2_CBR_SAY/src/main/java/com/ssafy/controller/MainController.java
+++ SafeFood_Web_Spring_gj_2_CBR_SAY/src/main/java/com/ssafy/controller/MainController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ssafy.model.dto.Food;
 import com.ssafy.model.service.EatService;
@@ -36,4 +38,28 @@ public class MainController {
 		return "index";
 	}
 	
+//	@PostMapping("/login")
+//	public String login(Model model, User user, RedirectAttributes redir, HttpServletResponse res,
+//			HttpSession session) {
+//
+//		// 3.뷰 연결
+//		User result = userService.login(user.getId(), user.getPassword());
+//
+//		logger.trace("result :{}", result);
+//		if (result != null
+//				&& (result.getId().equals(user.getId()) && result.getPassword().equals(user.getPassword()))) {
+//
+//			Cookie cookie = new Cookie("loginUser", user.getId());
+//			cookie.setPath("/");
+//			cookie.setMaxAge(60 * 2);
+//			res.addCookie(cookie);
+//			session.setAttribute("loginUser", user.getId());
+//			// redir.addFlashAttribute("alarm", "로그인 되었습니다.");
+//
+//			return "redirect:index";
+//		} else
+//			return "index";
+//
+//	}
+
 }
