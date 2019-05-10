26813855eb6af97da1f5a766c6660327a3b5465d
천보라 <capsulejay963@naver.com>
2019-05-10 17:56:52 +0900

    ddd

--- SafeFood_Web_Spring_gj_2_CBR_SAY/src/main/java/com/ssafy/controller/MainController.java
+++ SafeFood_Web_Spring_gj_2_CBR_SAY/src/main/java/com/ssafy/controller/MainController.java
@@ -16,8 +16,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.ssafy.model.dto.Eat;
 import com.ssafy.model.dto.Food;
 import com.ssafy.model.dto.User;
 import com.ssafy.model.service.EatService;
@@ -27,36 +29,19 @@ import com.ssafy.model.service.UserService;
 @Controller
 public class MainController {
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
-	
-	@Autowired 
+
+	@Autowired
 	UserService userService;
-	
-	@Autowired 
+
+	@Autowired
 	EatService eat;
-	
+
 	@Autowired
 	FoodService food;
-	
-	@GetMapping("/home")
-	public String home(Model model) {
-		logger.trace("home 방문.");
-		List<Food> foods = food.selectAll();
-		logger.trace("foods :: "+foods);
-		model.addAttribute("list", foods);
-		return "food/foodHome";
-	}
-	
-	
-	@GetMapping("/index")
-	public String index(Model model) {
-		logger.trace("index 방문.");
-		List<Food> foods = food.selectAll();
-		logger.trace("foods :: "+foods);
-		model.addAttribute("list", foods);
-		return "home";
-	}
-	
-	//로그인
+
+	/* ========================== User ========================================= */
+
+	// 로그인
 	@PostMapping("/login")
 	public String login(Model model, User user, RedirectAttributes redir, HttpServletResponse res,
 			HttpSession session) {
@@ -65,46 +50,46 @@ public class MainController {
 		logger.trace("user : {}", user);
 		User result = userService.login(user.getId(), user.getPw());
 		if (result != null) {
-			if(result.getAllergy() != null) {
+			if (result.getAllergy() != null) {
 				allergy_user = result.getAllergy().split(",");
 			}
 
 			StringBuilder my_allergy = new StringBuilder().append("[");
-			for(String al : allergy_user) {
+			for (String al : allergy_user) {
 				my_allergy.append('"').append(al).append('"').append(',');
 			}
 			my_allergy.append("]");
 			session.setAttribute("loginUser", result);
-			logger.trace(">>>  getAllergy : {}",result.getAllergy());
-			logger.trace("allergy : {}",Arrays.toString(allergy_user));
+			logger.trace(">>>  getAllergy : {}", result.getAllergy());
+			logger.trace("allergy : {}", Arrays.toString(allergy_user));
 			session.setAttribute("allergy", my_allergy.toString());
-		} else{ 
+		} else {
 			redir.addFlashAttribute("alarm", "아이디와 비밀번호를 확인해 주세요!");
 		}
 		return "redirect:home";
 	}
-	
-	//회원가입
+
+	// 회원가입
 	@GetMapping("/signUp")
-	public String signUpForm(Model model){
+	public String signUpForm(Model model) {
 		return "user/signUp";
 	}
-	
+
 	@PostMapping("/signUp")
 	public String doSignUp(Model model, User user, RedirectAttributes redir) {
 		try {
 			userService.insertUser(user);
 			redir.addFlashAttribute("alarm", "회원가입되었습니다. 로그인해주세요.");
 			return "redirect:/home";
-		}catch(DuplicateKeyException e) {
+		} catch (DuplicateKeyException e) {
 			logger.trace("DuplicateKeyException : {}", e);
 			model.addAttribute("alarm", "이미 존재하는 아이디입니다.");
 			model.addAttribute("prev", user);
 			return "user/signUp";
 		}
 	}
-	
-	//로그아웃
+
+	// 로그아웃
 	@GetMapping("/logout")
 	public String doLogout(Model model, HttpSession session, RedirectAttributes redir) {
 		User info = (User) session.getAttribute("loginUser");
@@ -113,35 +98,35 @@ public class MainController {
 		session.invalidate();
 		return "redirect:/home";
 	}
-	
-	//회원수정
+
+	// 회원수정
 	@GetMapping("/userInfo")
 	public String userInfoForm(Model model, HttpSession session, RedirectAttributes redir) {
 		return "user/userInfo";
 	}
-	
+
 	@PostMapping("/modify")
 	public String modify(Model model, RedirectAttributes redir, User user, HttpSession session) {
 		logger.trace("modify: {}", user.getId());
 		String[] allergy_user = null;
 		int result = userService.updateUser(user);
-		
+
 		if (result > 0) {
 			session.setAttribute("loginUser", user);
 			allergy_user = user.getAllergy_arr();
-			
+
 			StringBuilder my_allergy = null;
-			if(allergy_user != null) {
+			if (allergy_user != null) {
 				my_allergy = new StringBuilder().append("[");
-				for(String al : allergy_user) {
+				for (String al : allergy_user) {
 					my_allergy.append('"').append(al).append('"').append(',');
 				}
 				my_allergy.append("]");
 				session.setAttribute("allergy", my_allergy.toString());
-			}else {
-				session.setAttribute("allergy","");
+			} else {
+				session.setAttribute("allergy", "");
 			}
-			
+
 			redir.addFlashAttribute("alarm", "회원정보 수정 완료하였습니다.");
 		} else {
 			redir.addFlashAttribute("alarm", "회원정보 수정 실패하였습니다.");
@@ -149,48 +134,48 @@ public class MainController {
 		return "redirect:/home";
 	}
 
-	//탈퇴
+	// 탈퇴
 	@GetMapping("/withdraw")
 	public String doWithdraw(Model model, RedirectAttributes redir, User user, HttpSession session) {
 		User users = (User) session.getAttribute("loginUser");
 		logger.trace("logout: {}", users.getId());
-		
+
 		int result = userService.deleteUser(users.getId());
-		
+
 		if (result > 0) {
 			redir.addFlashAttribute("alarm", "회원탈퇴되었습니다.");
 			session.invalidate();
-		}else {
+		} else {
 			redir.addFlashAttribute("alarm", "회원실패하였습니다.");
 		}
 		return "redirect:/home";
 	}
-	
-	//아이디 찾기
+
+	// 아이디 찾기
 	@GetMapping("/userfindId")
 	public String userfindIdForm(Model model) {
 		return "user/userfindId";
-	}	
-	
-	//비밀번호 찾기
+	}
+
+	// 비밀번호 찾기
 	@GetMapping("/userfindPw")
 	public String userfindPwForm(Model model) {
 		return "user/userfindPw";
 	}
-	
+
 	@PostMapping("/userfindPw")
 	public String doUserfindPw(Model model, RedirectAttributes redir, User user, HttpSession session, String div) {
 		User users = (User) session.getAttribute("loginUser");
-		
-		logger.trace(">>> email {} :: {}",users.getEmail(), user.getEmail());
+
+		logger.trace(">>> email {} :: {}", users.getEmail(), user.getEmail());
 		userService.findPw(users);
-		if(users.getEmail().equals(user.getEmail())) { 
+		if (users.getEmail().equals(user.getEmail())) {
 			userService.sendEmail(users, div);
 			redir.addFlashAttribute("alarm", "임시비밀번호를 메일로 발송했습니다.");
-			
-		}else { 
+
+		} else {
 			redir.addFlashAttribute("alarm", "잘못된 이메일입니다.");
-			
+
 		}
 		return null;
 		/*
@@ -201,32 +186,39 @@ public class MainController {
 		 * "아이디 또는 패스워드가 다릅니다."); return "user/userfindPw"; }
 		 */
 	}
-	
-	//비밀번호 재설정
+
+	// 비밀번호 재설정
 	@PostMapping("/doePw")
 	public String doRePw(Model model, RedirectAttributes redir, User user, HttpSession session) {
 		int result = userService.updatePw(user);
-		if(result > 0) {
+		if (result > 0) {
 			redir.addFlashAttribute("alarm", "비밀번호가 변경되었습니다.");
 			return "redirect:/home";
-		}else {
+		} else {
 			redir.addFlashAttribute("alarm", "비밀번호가 다릅니다.");
 			return "user/userfindPw";
 		}
 	}
-	//에러 페이지 연결
-	@RequestMapping("/error")
-	public String errorHandling(Model model) {
-		return "include/Error";
+
+	/* ========================== Food ========================================= */
+
+	@GetMapping("/home")
+	public String home(Model model) {
+		logger.trace("home 방문.");
+		List<Food> foods = food.selectAll();
+		logger.trace("foods :: " + foods);
+		model.addAttribute("list", foods);
+		return "food/foodHome";
 	}
-	
+
 	@PostMapping("/search")
 	public String search(Model model, HttpServletRequest req, RedirectAttributes redir) {
 		logger.trace("search 방문.");
 		String by = req.getParameter("by");
 		List<Food> result = null;
 		if (by.equals("상품명")) {
-			String name = req.getParameter("search_input");;
+			String name = req.getParameter("search_input");
+			;
 			result = food.selectName2(name);
 		} else if (by.equals("제조사")) {
 			String maker = req.getParameter("search_input");
@@ -238,17 +230,60 @@ public class MainController {
 		model.addAttribute("list", result);
 		return "food/foodHome";
 	}
-	
+
 	@GetMapping("/productinfo")
 	public String productinfo(Model model, HttpSession session) {
 		logger.trace("productinfo 방문.");
-		User user = (User)session.getAttribute("loginUser");
+		User user = (User) session.getAttribute("loginUser");
 		List<Food> foods = eat.searchMyList(user.getId());
-		logger.trace("foods :: "+foods);
+		logger.trace("foods :: " + foods);
 		model.addAttribute("list", foods);
 		return "food/productinfo";
 	}
-	
+
+	@GetMapping("/detail")
+	public String foodDetail(Model model, HttpServletRequest req) {
+		logger.trace("foodDetail 방문.");
+		String name = req.getParameter("name");
+		Food food1 = food.selectName1(name);
+		logger.trace("food :: " + food1);
+		model.addAttribute("food", food1);
+		return "food/detailinfo";
+	}
+
+	@ResponseBody
+	@PostMapping("/chart")
+	public Food chart(Model model, int code) {
+		Food foods = food.selectCode(code);
+		logger.trace("chart :: " + foods);
+		return foods;
+	}
+
+	/* ========================== Eat ========================================= */
+
+	@GetMapping("/addEat")
+	public String addEat(Model model, int code, RedirectAttributes redir, HttpSession session) {
+		logger.trace("addEat : {}");
+		User info = (User) session.getAttribute("loginUser");
+		Eat eatNew = new Eat(info.getId(), code);
+		eat.insert(eatNew);
+		return "redirect:home";
+	}
+
+	/* ========================== Like ========================================= */
+
+	/*
+	 * ========================== Review =========================================
+	 */
+
+	/* ========================== etc ========================================= */
+
+	// 에러 페이지 연결
+	@RequestMapping("/error")
+	public String errorHandling(Model model) {
+		return "include/Error";
+	}
+
 	@GetMapping("/qna")
 	public String qna(Model model) {
 		return "menu/qna";
